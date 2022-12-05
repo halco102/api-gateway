6e1f06acd16ee9570745b6f7b36d8f1db2d43ed4
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-05 13:41:29 +0100

    post api

--- src/main/java/com/api/gateway/controler/PostController.java
+++ src/main/java/com/api/gateway/controler/PostController.java
@@ -1,25 +1,51 @@
 package com.api.gateway.controler;
 
+import com.api.gateway.dto.post.post.request.EditPostRequest;
 import com.api.gateway.dto.post.post.request.PostRequestDto;
 import com.api.gateway.feign.post.PostClient;
+import com.api.gateway.security.AuthTokenFilter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/post")
+@Slf4j
 public class PostController {
 
     private final PostClient postClient;
 
     @PostMapping
-    public ResponseEntity<?> savePost(@RequestBody PostRequestDto requestDto) {
-        return new ResponseEntity<>(postClient.savePost(requestDto), HttpStatus.OK);
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+    public ResponseEntity<?> savePost(@RequestBody PostRequestDto requestDto, HttpServletRequest request) {
+        return new ResponseEntity<>(postClient.savePost(requestDto, AuthTokenFilter.parseJwt(request)), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllPosts() {
+        return new ResponseEntity<>(postClient.getAllPosts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPostById(@PathVariable Long id) {
+        return new ResponseEntity<>(postClient.getPostById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> getAllUserPosts(@PathVariable Long id) {
+        return new ResponseEntity<>(postClient.getAllUsersPost(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
+    public ResponseEntity<?> editPostById(@PathVariable Long id, @RequestBody EditPostRequest request, HttpServletRequest servletRequest) {
+        return new ResponseEntity<>(postClient.editPostById(id, request, AuthTokenFilter.parseJwt(servletRequest)), HttpStatus.OK);
     }
 
 
--- src/main/java/com/api/gateway/feign/post/PostClient.java
+++ src/main/java/com/api/gateway/feign/post/PostClient.java
@@ -1,19 +1,35 @@
 package com.api.gateway.feign.post;
 
 import com.api.gateway.dto.post.post.PostDto;
+import com.api.gateway.dto.post.post.request.EditPostRequest;
 import com.api.gateway.dto.post.post.request.PostRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "postClient", url = "http://localhost:8086/api/v1/post")
+import javax.validation.Valid;
+import java.util.List;
+
+@FeignClient(name = "postClient", url = "${POST_MICROSERVICE}")
 public interface PostClient {
 
     @PostMapping(value = "", consumes = {  MediaType.APPLICATION_JSON_VALUE,
             MediaType.MULTIPART_FORM_DATA_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE})
-    PostDto savePost(@RequestBody PostRequestDto requestDto);
+    PostDto savePost(@RequestBody PostRequestDto requestDto, @RequestHeader("Bearer") String token);
+
+
+    @GetMapping
+    List<PostDto> getAllPosts();
+
+    @GetMapping("/{id}")
+    PostDto getPostById(@PathVariable Long id);
+
+    @GetMapping("/user/{id}")
+    List<PostDto> getAllUsersPost(@PathVariable Long id);
+
+    @PutMapping("/{id}")
+    PostDto editPostById(@PathVariable Long id, @RequestBody EditPostRequest request, @RequestHeader("Bearer") String jwt);
 
 }
