4a4d10ebafd72a75f90db9795e5905920ad659a7
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-05 13:42:03 +0100

    adding set of roles for user

---
+++ src/main/java/com/api/gateway/dto/user/response/RoleDto.java
@@ -0,0 +1,16 @@
+package com.api.gateway.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDto {
+
+    private Long id;
+
+    private String name;
+
+}
--- src/main/java/com/api/gateway/dto/user/response/UserSecurityDto.java
+++ src/main/java/com/api/gateway/dto/user/response/UserSecurityDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,7 +22,7 @@ public class UserSecurityDto {
 
     private String imageUrl;
 
-    private UserRole userRole;
+    private Set<RoleDto> userRoles;
 
 
 }
